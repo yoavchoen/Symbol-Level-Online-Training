ed26daa35d21b7caa512183d2a9225ba6b7376dd
Yoav Cohen <75442295+yoavchoen@users.noreply.github.com>
2021-07-23 17:35:57 +0300

    Update graphs_dataloader_and_plot.py

--- graphs_dataloader_and_plot.py
+++ graphs_dataloader_and_plot.py
@@ -3,6 +3,12 @@ from graph_plot_func import *
 # import numpy as np
 # import matplotlib.pyplot as plt
 
+"""
+1. Saving results simulation DATA (variables) to binary files.
+2. Loading the DATA and plot the results.
+"""
+
+#---success rate---#
 # pickle_success_rate = open("pickle_success_rate.pickle", "wb")
 # pickle.dump(d_path1, pickle_success_rate)
 # pickle_success_rate.close()
@@ -11,6 +17,8 @@ d_path1 = pickle.load(pickle_success_rate_in)
 pickle_success_rate_in.close()
 success_rate_plot(d_path1)
 
+
+#---ViterbiNet---#
 # import pickle
 # pickle_ViterbiNet = open("pickle_ViterbiNet.pickle", "wb")
 # pickle.dump([m_fSERAvg, v_fSigWdB], pickle_ViterbiNet)
@@ -20,6 +28,9 @@ pickle_ViterbiNet_in = open("pickle_ViterbiNet.pickle", "rb")
 pickle_ViterbiNet_in.close()
 ViterbiNet_plot(v_fSigWdB, m_fSERAvg)
 
+
+#---channel tracking simulation results (median point chosen to be optimal for SNR=6dB)---:
+#---channel coefficients---#
 # import pickle
 # pickle_channel_taps = open("pickle_channel_taps.pickle", "wb")
 # pickle.dump(m_fChannel, pickle_channel_taps)
@@ -29,6 +40,8 @@ m_fChannel = pickle.load(pickle_channel_taps_in)
 pickle_channel_taps_in.close()
 channel_taps_plot(m_fChannel)
 
+
+#---moving average of SER---#
 # import pickle
 # pickle_track_res = open("pickle_track_res.pickle", "wb")
 # pickle.dump([track_SER_fullCSI, track_SER_perfectCSI, track_SER_uncertaintyCSI, v_fSigWdB], pickle_track_res)
@@ -36,8 +49,11 @@ channel_taps_plot(m_fChannel)
 pickle_track_res_in = open("pickle_track_res.pickle", "rb")
 [track_SER_fullCSI, track_SER_perfectCSI, track_SER_uncertaintyCSI, v_fSigWdB] = pickle.load(pickle_track_res_in)
 pickle_track_res_in.close()
-track_res_plot(track_SER_fullCSI, track_SER_perfectCSI, track_SER_uncertaintyCSI, v_fSigWdB)
+dB_wanted = 6  #chosen SNR results [-6,-4,-2,0,2,4,6,8,10]
+track_res_plot(track_SER_fullCSI, track_SER_perfectCSI, track_SER_uncertaintyCSI, v_fSigWdB, dB_wanted)
 
+
+#---AVG display - for each SNR---#
 # import pickle
 # pickle_AVGtrack_res = open("pickle_AVGtrack_res.pickle", "wb")
 # pickle.dump([track_SER_avg, v_fSigWdB], pickle_AVGtrack_res)
@@ -47,6 +63,8 @@ pickle_AVGtrack_res_in = open("pickle_AVGtrack_res.pickle", "rb")
 pickle_AVGtrack_res_in.close()
 AVGtrack_plot(track_SER_avg, v_fSigWdB)
 
+
+#---diversity MSE---#
 # import pickle
 # pickle_diversity = open("pickle_diversity.pickle", "wb")
 # pickle.dump([dict_train, dict_bad_train, dict_med_train,
@@ -59,6 +77,8 @@ pickle_diversity_in.close()
 diversity_plot(dict_train, dict_bad_train, dict_med_train,
              MSE_diversity_train, MSE_diversity_bad_train, MSE_diversity_med_train)
 
+
+#---noise power & error---#
 # import pickle
 # pickle_NoisePower_error = open("pickle_NoisePower_error.pickle", "wb")
 # pickle.dump([NOISE_train, NOISE_bad_train, NOISE_med_train,
@@ -69,3 +89,7 @@ pickle_NoisePower_error_in = open("pickle_NoisePower_error.pickle", "rb")
              ERR_train, ERR_bad_train, ERR_med_train] = pickle.load(pickle_NoisePower_error_in)
 pickle_NoisePower_error_in.close()
 NoisePower_error(NOISE_train, NOISE_bad_train, NOISE_med_train, ERR_train, ERR_bad_train, ERR_med_train)
+
+plt.show(block=True)
+print('Simulation results')
+print('')
